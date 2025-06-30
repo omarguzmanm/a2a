cc55098ddcf376d6ff4974ad7806358533040eb6
Omar Alejandro Guzmán Munguía <omaralexguzmanm21@gmail.com>
2025-06-29 22:26:13 -0600

    refactor: use secuential process in the agent network

--- network.py
+++ network.py
@@ -3,25 +3,15 @@ import asyncio
 
 async def main():
     network = AgentNetwork()
-    network.add("issue-analysis", "http://localhost:5001")
-    network.add("code-generator", "http://localhost:5002")
-    network.add("docs-generator", "http://localhost:5003")
+    network.add("issue-analysis", "http://localhost:5000")
+    network.add("code-generator", "http://localhost:5001")
+    network.add("docs-generator", "http://localhost:5002")
     
     flow = Flow(agent_network=network, name="Solve issues with code generation and documentation")
     flow.ask("issue-analysis", "{issue}")
+    flow.ask("code-generator", "{latest_result}")
+    flow.ask("docs-generator", "{latest_result}")
     
-    parallel_results = (flow.parallel()
-        .ask("code-generator", "{latest_result}")
-        .branch()
-        .ask("docs-generator", "{latest_result}")
-        .end_parallel(max_concurrency=2))
-    
-    flow.execute_function(
-        lambda results, context: f"Code: {results['1']}\nDocs : {results['2']}",
-        parallel_results
-    )
-    
-    # Execute the workflow
     result = await flow.run({
         "issue": "Create a simple Python script that prints 'Hello, World!'"
     })
